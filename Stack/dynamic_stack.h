1cbe027128078761cc09a2b9f7a7528fae8cdf98
Sucipto-Abdullah <ciptoabdul23@gmail.com>
2025-05-19 11:55:35 +0700

    stack uploaded

---
+++ Stack/dynamic_stack.h
@@ -0,0 +1,70 @@
+#ifndef stack_H
+#define stack_H
+
+#include <iostream>
+
+typedef char infotype;
+typedef struct Element_Stack *address;
+
+struct Element_Stack{
+	infotype info;
+	address next;
+};
+
+address alokasi(infotype info){
+	address s;
+	s = new Element_Stack;
+	s->info = info;
+	s->next = NULL;
+	return s;
+}
+
+struct stack{
+	address top;
+
+	void create(){
+		top = NULL;
+
+	}
+	void push(infotype nilai){
+		address nilai_baru = alokasi(nilai);
+		if(top == NULL){
+			top = nilai_baru;
+		}
+		else{
+			nilai_baru->next = top;
+			top = nilai_baru;
+		}
+	}
+	void pop(){
+		if(top != NULL){
+			address target = top;
+			top = top->next;
+			target->next = NULL;
+			delete target;
+		}
+
+	}
+	address get_address(infotype nilai){
+		address index = top;
+		while(index->info != nilai && index != NULL){
+			index = index->next;
+		}
+		return index;
+	}
+	
+	void read(){
+		address index = top;
+		while(index != NULL){
+			if(index->next != NULL){
+				std::cout << index->info << ", ";
+			}else{
+				std::cout << index->info << " ";
+			}
+			index = index->next;
+		}
+		std::cout << "\n";
+	}
+};
+
+#endif
---
+++ Stack/main.cpp
@@ -0,0 +1,6 @@
+#include "Binner_converter.h"
+
+int main(){
+    std::cout << convert_binner(21) << "\n";
+
+}
