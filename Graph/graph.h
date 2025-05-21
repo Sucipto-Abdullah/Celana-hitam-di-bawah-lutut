1deb65e0611b6904dcb77fb23c6ede189bbd2990
Sucipto-Abdullah <ciptoabdul23@gmail.com>
2025-05-21 08:07:28 +0700

    graph updated

--- Graph/graph.h
+++ Graph/graph.h
@@ -9,16 +9,18 @@ typedef struct edge *e;
 typedef struct Graph *graph;
 
 struct point{
+    std::string name;
     int x;
     int y;
     char symbol;
     p next;
 };
 
-p point_alocate(int x, int y, char symbol){
+p point_alocate(std::string name, int x, int y, char symbol){
 	p pnt;
 	pnt = new point;
 	pnt->next = NULL;
+    pnt->name = name;
 	pnt->x = x;
 	pnt->y = y;
 	pnt->symbol = symbol;
@@ -43,103 +45,86 @@ e edge_alocate( p point_1, p point_2 ){
     return new_edge;
 }
 
-struct point_queue{
-    p top;
-    p tail;
-    
-
-    void create(){
-        top = NULL;
-        tail = NULL;
-    }
-
-    void push( p value ){
-        if( top == NULL ){
-            top = value;
-            tail = value;
-        }else{
-            tail->next = value;
-            tail = value;
-        }
-    }
-
-    void pop(){
-        if( top != NULL ){
-            p target = top;
-            top = target->next;
-            target->next = NULL;
-            delete target;
-        }
-    }
-};
-
-struct edge_queue{
-    private:
-    bool created = false;
-    public:
-    e top;
-    e tail;
-
-    void create(){
-        if (!created){
-            top = NULL;
-            tail = NULL;
-            created = true;
-        }
-    }
-    void push( e edge ){
-        if( created){
-            if( top == NULL ){
-                top = edge;
-                tail = edge;
-            }else{
-                tail->next = edge;
-                tail = edge;
-            }
-        }
-    }
-};
 
 struct Graph{
-    int width, height;
+    
+    private:
+    p top_point = NULL;
+    e top_edge = NULL;
 
+    int edge_filled = 0;
+
+    
+    public:
+    int width, height;
+    
     Graph(int Width, int Height){
         width = Width;
         height = Height;
     }
 
-    void render_point(point_queue point_queue, edge_queue edge_queue){
+    void insertVertex( p point )
+    {
+        if (top_point == NULL){
+            top_point = point;
+        }else{
+            point->next = top_point;
+            top_point = point;
+        }
+    }
+    void insertEdge( e edge ){
+        if (top_edge == NULL){
+            top_edge = edge;
+        }
+        else{
+            edge->next = top_edge;
+            top_edge = edge;
+        }
+        edge_filled++;
+    }
 
-        p render_target = point_queue.top;
-        e edge_target = edge_queue.top;
+    void display_map()
+    {
+        p vertex_index = top_point;
+        e edge_index = top_edge;
 			
         for (int pixel_y = 1; pixel_y < height+1 ; pixel_y ++ ){
         	
             for( int pixel_x = 1; pixel_x < width+1; pixel_x ++ ){
                 
-                while ( render_target != NULL ){
-                   	if( render_target->x == pixel_x && render_target->y == pixel_y ){
-                   		std::cout<< " " << render_target->symbol << " ";
+                while ( vertex_index != NULL ){
+                   	if( vertex_index->x == pixel_x && vertex_index->y == pixel_y ){
+                   		std::cout<< " " << vertex_index->symbol << " ";
                    		break;
 					}
-					if( render_target->next == NULL ){
+					if( vertex_index->next == NULL ){
 						std::cout << " . ";
 					}
 					
-					render_target = render_target->next;
+					vertex_index = vertex_index->next;
 				}
-				render_target = point_queue.top;
+				vertex_index = top_point;
                 
             }
             std::cout << "\n";
         }
         
-//        std::cout << edge_target->length << "\n";
-           
-        while(edge_target != NULL){
-            std::cout << edge_target->point[0]->symbol << " - " << edge_target->length << "km - " << edge_target->point[1]->symbol << "\n";
-            edge_target = edge_target->next;
+    }
+
+    void display_table(){
+        e edge_index = top_edge;
+        int number_index = 1;
+
+        for (int i=0; i<edge_filled; i++)
+        {
+            std::cout << "Jalur " << i+1 << "\n";
+            std::cout << "\ttitik pertama: " << edge_index->point[0]->name << " ( " << edge_index->point[0]->symbol << " )\n";
+            std::cout << "\ttitik kedua: " << edge_index->point[1]->name << " ( " << edge_index->point[1]->symbol << " )\n";
+            std::cout << "\tJarak kedua titik: " << edge_index->length << "Km\n\n";
+            edge_index = edge_index->next;
+            number_index ++;
         }
+        
     }
 
 };
--- Graph/main.cpp
+++ Graph/main.cpp
@@ -2,37 +2,34 @@
 
 int main(){
 
-    p rumah = point_alocate(2, 4, 'H');
-    p kampus = point_alocate(5, 5, 'K');
-	p spbu = point_alocate(3, 7, 'S');
-	p masjid = point_alocate(11, 15, 'M');
-	p tol = point_alocate(7, 6, 'T');
+    p rumah = point_alocate("rumah", 2, 4, 'H');
+    p kampus = point_alocate("Kampus", 5, 5, 'K');
+	p spbu = point_alocate("SPBU" , 3, 7, 'S');
+	p masjid = point_alocate( "Masjid", 11, 15, 'M');
+	p tol = point_alocate( "Tol", 7, 6, 'T');
 	
 	e jalur_a = edge_alocate(rumah, spbu);
 	e jalur_b = edge_alocate(rumah, kampus);
 	e jalur_c = edge_alocate(rumah, masjid);
 	e jalur_d = edge_alocate( kampus, tol );
 	
-    point_queue point_queue;
-    point_queue.create();
-    point_queue.push(rumah);
-    point_queue.push(kampus);
-    point_queue.push(spbu);
-    point_queue.push( masjid );
-    point_queue.push(tol);
-	
-	edge_queue edge_queue;
-	edge_queue.create();
-	edge_queue.push(jalur_a);
-	edge_queue.push(jalur_b);
-	edge_queue.push(jalur_c);
-	edge_queue.push(jalur_d);
-	
     graph screen = new Graph(20, 20);
-	
-    screen->render_point(point_queue, edge_queue);
+
+    screen->insertVertex(rumah);
+    screen->insertVertex(kampus);
+    screen->insertVertex(spbu);
+    screen->insertVertex(masjid);
+    screen->insertVertex(tol);
+
+    screen->insertEdge(jalur_a);
+    screen->insertEdge(jalur_b);
+    screen->insertEdge(jalur_c);
+    screen->insertEdge(jalur_d);
+
+    screen->display_map();
+    screen->display_table();
     
-    std::cout << edge_queue.top->length;
+    // std::cout << edge_queue.top->length;
 
     return 0;
 }
